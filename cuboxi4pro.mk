ce1d6024db8b04a687afa5e643447be9cb7ed3d0
Humberto Borba <humberos@gmail.com>
2014-10-10 01:17:51 -0300

    cuboxi4pro: initial commit
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I9cc8578ce73cefa31ce0f0b5d8d1d660daa3d1ff

---
+++ cuboxi4pro.mk
@@ -0,0 +1,27 @@
+#
+# Copyright 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/fsl/cuboxi4pro/device.mk)
+
+PRODUCT_NAME := cuboxi4pro
+PRODUCT_DEVICE := cuboxi4pro
+PRODUCT_BRAND := Freescale
+PRODUCT_MODEL := cuboxi4pro
+PRODUCT_MANUFACTURER := Freescale
---
+++ device.mk
@@ -0,0 +1,31 @@
+#
+# Copyright 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/fsl/cuboxi-common/common.mk)
+
+DEVICE_PATH := device/fsl/cuboxi4pro
+
+# media config
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf \
+    $(DEVICE_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(DEVICE_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(DEVICE_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+
+# set build fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Freescale/cuboxi4pro/cuboxi4pro:4.4.4/KTU84P/eng.humberos.20141010.142909:userdebug/dev-keys \
+    PRIVATE_BUILD_DESC="cuboxi4pro-userdebug 4.4.4 KTU84P eng.humberos.20141010.142909 dev-keys"
